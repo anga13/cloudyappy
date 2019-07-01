6c8e8c050ad113eaf0f6b46dc599c21cc3f16b70
anga13 <kalle.spik@hotmail.com>
2019-07-01 02:00:31 +0200

    Use manual mock for DB module

---
+++ app/__mocks__/db.js
@@ -0,0 +1,29 @@
+const logger = require('pino')()
+
+module.exports = {
+    async allEmployees() {
+        return employees
+    },
+    async findEmployee(id) {
+	    const found = employees.find(employee => {
+		    return employee.id === id
+	    })
+	    return found
+    }
+}
+
+const employees = [
+	{
+            id: 33,
+            firstName: 'Barney',
+            lastName: 'Rubble'
+        }, {
+            id: 235231,
+            firstName: 'Fred',
+            lastName: 'Flintstone'
+        }, {
+            id: 7643,
+            firstName: 'Mr',
+            lastName: 'Slate'
+        }
+]
