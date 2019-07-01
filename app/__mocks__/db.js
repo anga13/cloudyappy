23c3c2b973989dca3a45bb74f3b96b85deedfaaf
anga13 <kalle.spik@hotmail.com>
2019-07-01 02:05:03 +0200

    Lint fix

--- app/__mocks__/db.js
+++ app/__mocks__/db.js
@@ -1,29 +1,27 @@
-const logger = require('pino')()
-
 module.exports = {
     async allEmployees() {
         return employees
     },
     async findEmployee(id) {
-	    const found = employees.find(employee => {
-		    return employee.id === id
-	    })
-	    return found
+        const found = employees.find(employee => {
+            return employee.id === id
+        })
+        return found
     }
 }
 
 const employees = [
-	{
-            id: 33,
-            firstName: 'Barney',
-            lastName: 'Rubble'
-        }, {
-            id: 235231,
-            firstName: 'Fred',
-            lastName: 'Flintstone'
-        }, {
-            id: 7643,
-            firstName: 'Mr',
-            lastName: 'Slate'
-        }
+    {
+        id: 33,
+        firstName: 'Barney',
+        lastName: 'Rubble'
+    }, {
+        id: 235231,
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+    }, {
+        id: 7643,
+        firstName: 'Mr',
+        lastName: 'Slate'
+    }
 ]
--- app/controller.test.js
+++ app/controller.test.js
@@ -1,4 +1,3 @@
-const db = require('./db')
 const controller = require('./controller')
 jest.mock('./db')
 
